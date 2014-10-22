6f36b3ba48d86bd5d183a0ec83280de9e07d934b
Michael Cannon <mc@aihr.us>
2014-10-22 11:37:51 -0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+/** Start the engine */
+require_once( get_template_directory() . '/lib/init.php' );
+
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', 'OPI Living' );
+define( 'CHILD_THEME_URL', 'http://www.allstagesmarketing.com' );
+
+/** Add support for custom background */
+add_custom_background();
+
+add_action('wp_head','print_print');
+function print_print() { ?>
+       <link rel="stylesheet" type="text/css" href="http://www.optimumperformanceinstitute.com/wp-content/themes/roanne-program/print.css" media="print" />
+<?php } 
+
+/** Add support for 3-column footer widgets */
+add_theme_support( 'genesis-footer-widgets', 4 );
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'name' => 'Above',
+'before_widget' => '<div class="after-content-left">',
+'after_widget' => '</div>',
+));
+}
+add_action('genesis_before_content_sidebar_wrap', 'insert_mysidebar');
+function insert_mysidebar() {
+if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Above') );
+}
+
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'name' => 'After Content',
+'before_widget' => '<div class="after-content-right">',
+'after_widget' => '</div>',
+));
+}
+add_action('genesis_before_content_sidebar_wrap', 'insert_myevents');
+function insert_myevents() {
+if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('After Content') );
+}
+/** Modify the speak your mind text */
+add_filter( 'genesis_comment_form_args', 'custom_comment_form_args' );
+function custom_comment_form_args($args) {
+    $args['title_reply'] = 'Leave a Comment';
+    return $args;
+}
+remove_action('genesis_footer', 'genesis_do_footer');
+function do_custom_footer() {
+	?>
+    <div class="footer-credits">
+    <p style="font-size:.9em;text-align:center;">Copyright © <?php echo date('Y'); ?> <a href="http://www.optimumperformanceinstitute.com/" title="THE OPTIMUM PERFORMANCE INSTITUTE - Young Adult Programs">The Optimum Performance Institute</a>. All rights reserved. <a href="http://www.allstagesmarketing.com/" title="Web Design">Web Design </a>by <a href="http://www.allstagesmarketing.com/" title="All Stages Marketing"> All Stages Marketing.</a></p></div>
+<?php
+}
+add_action('genesis_footer', 'do_custom_footer');
+add_action('genesis_after_post_content', 'do_links');
+function do_links() {
+	if(get_field('left_link')): ?> 
+<?php foreach(get_field('left_link') as $post_object): ?>
+   <a href="<?php echo get_permalink($post_object->ID); ?>" title="<?php echo get_the_title($post_object->ID) ?>" class="left">«  <?php echo get_the_title($post_object->ID) ?></a>
+<?php endforeach;
+        endif;
+	if(get_field('right_link')):
+		foreach(get_field('right_link') as $post_object): ?>
+   <a href="<?php echo get_permalink($post_object->ID); ?>" title="<?php echo get_the_title($post_object->ID) ?>" class="right"><?php echo get_the_title($post_object->ID) ?>  »</a>
+<?php endforeach;
+        endif;
+}
---
+++ home.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Template Name: home
+ * WARNING: This file is part of the core Genesis framework. DO NOT edit
+ * this file under any circumstances. Please do all modifications
+ * in the form of a child theme.
+ *
+ * This file handles posts, but only exists for the sake of
+ * child theme forward compatibility.
+ *
+ * @package Genesis
+ */
+add_action('wp_head','print_styles');
+function print_styles() { ?>
+       <link rel="stylesheet" type="text/css" href="http://www.optimumperformanceinstitute.com/wp-content/themes/roanne-program/wt-rotator.css"/>
+<?php }
+add_action('wp_head', 'my_load_scripts');
+function my_load_scripts() {
+	?>
+   <script type="text/javascript" src="http://www.optimumperformanceinstitute.com/wp-content/themes/roanne-program/js/jquery-1.6.1.min.js"></script>
+    <script type="text/javascript" src="http://www.optimumperformanceinstitute.com/wp-content/themes/roanne-program/js/jquery.easing.1.3.min.js"></script>
+    <script type="text/javascript" src="http://www.optimumperformanceinstitute.com/wp-content/themes/roanne-program/js/jquery.wt-rotator.min.js"></script>
+    <?
+}
+add_action('wp_head', 'my_raw_script');
+function my_raw_script() {
+    echo <<<JS
+        <script type="text/javascript">
+    	$(document).ready(	
+			function() {
+				$(".container1").wtRotator({
+					width:904,
+					height:386,
+					thumb_width:16,
+            		thumb_height:18,
+					button_width:16,
+					button_height:18,
+					button_margin:3,
+					auto_start:true,
+					delay:4000,
+					play_once:false,
+					transition:"fade",
+					transition_speed:800,
+					auto_center:false,
+					easing:"",
+					cpanel_position:"outside",
+					cpanel_align:"BR",
+					timer_align:"top",
+					display_thumbs:false,
+					display_dbuttons:false,
+					display_playbutton:false,
+					display_thumbimg:false,
+           			display_side_buttons:true,
+					display_numbers:true,
+					display_timer:false,
+					mouseover_pause:true,
+					cpanel_mouseover:false,
+					text_mouseover:false,
+					text_effect:"fade",
+					text_sync:true,
+					tooltip_type:"none",
+					lock_tooltip:true,
+					shuffle:false,
+					block_size:75,
+					vert_size:25,
+					horz_size:50,
+					block_delay:25,
+					vstripe_delay:75,
+					hstripe_delay:180			
+				});
+			}
+		);
+		        </script>
+JS;
+}
+?>
+<?php
+add_action('genesis_before_content_sidebar_wrap', 'child_do_ex_content');
+function child_do_ex_content() { ?>
+<div class="container1">
+	<div class="wt-rotator">
+    	<div class="screen">
+            <noscript>
+            	<!-- placeholder 1st image when javascript is off -->
+                <img src="<?php the_field('photo'); ?>" alt="OPI Living" />
+            </noscript>
+      	</div>
+        <div class="c-panel">
+      		<div class="thumbnails">
+                <ul>
+	<?php while(the_repeater_field('slideshow')): ?>
+    
+                               <li>
+              <a href="<?php echo get_sub_field('photo'); ?>" /></a>
+              <a href="<?php echo get_sub_field('page_link'); ?>" /></a>
+                   <div style="top:280px; left:110px; width:520px; height:0; color:#333; background:none; font-size:20px; text-align:center;">           
+					<?php the_sub_field('description'); ?>
+                    </div> 
+</li>
+    <?php endwhile; ?>
+              	</ul>
+          	</div>   
+        </div>
+    </div>	
+</div>
+<?php 
+}
+genesis();
